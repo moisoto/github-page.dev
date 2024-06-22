d5817aba19835601267e0dce1d7f9e5f2f3078d2
Moises Soto <35436843+moisoto@users.noreply.github.com>
2024-06-22 00:39:17 -0400

    Now fixes detached head on public.

--- init_submodules.sh
+++ init_submodules.sh
@@ -9,3 +9,16 @@ if [[ -d $submodule ]] ; then
 fi
 
 git submodule update --init
+if [[ -d $submodule ]] ; then
+	cd ${submodule}
+	echo "\033[0;32mFixing Tracking for Submodule ${submodule}\033[0m"
+	git checkout -b custom --track origin/custom
+	cd -
+fi
+
+submodule=public
+if [[ -d $submodule ]] ; then
+	echo "\033[0;32mFixing Tracking for Submodule ${submodule}\033[0m"
+	cd ${submodule}
+	git switch main
+fi
